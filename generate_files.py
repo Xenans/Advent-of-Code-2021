8230016cc63a0db41083cf027afbbbac5709c095
Xenans <xenansgaming@gmail.com>
2021-12-21 01:31:14 -0500

    Updated boilerplate utility

--- generate_files.py
+++ generate_files.py
@@ -3,9 +3,10 @@ for i in range(25):
     day = i + 1
     try:
         os.mkdir(f'./Day {day}')
+        with open(f'./Day {day}/input.txt', 'a+') as file:
+            pass
+        with open(f'./Day {day}/day{day}.py', 'a+') as file:
+            file.write(f"""file = open('./Day {day}/input.txt', 'r')
+                            input = [line.strip() for line in file]""")
     except:
-        pass
-    with open(f'./Day {day}/input.txt', 'a+') as file:
-        pass
-    with open(f'./Day {day}/day{day}.py', 'a+') as file:
         pass
