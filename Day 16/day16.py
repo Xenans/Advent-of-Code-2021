391b2ef2f3c5afb12753423c0a6fb9310ce1a421
Xenans <xenansgaming@gmail.com>
2021-12-21 19:49:12 -0500

    Day 16! Part 2!

--- Day 16/day16.py
+++ Day 16/day16.py
@@ -18,49 +18,89 @@ def pop_literal(data):
         data = data[5:]
         end = group[0] == '0'
         literal += group[1:]
-    return data, literal
+    value = [int(literal, 2)]
+    return data, value
 
 
 def pop_operator(data):
     length_type = data[0]
     data = data[1:]
     version_sum = 0
+    values = []
     if length_type == '0':
         # Next 15 bits indicate total length of bits
         length = int(data[:15], 2)
         data = data[15:]
         consumed_data = data[:length]
         while consumed_data:
-            consumed_data, temp_version = pop_packet(consumed_data)
+            consumed_data, temp_values, temp_version = pop_packet(
+                consumed_data)
             version_sum += temp_version
+            values.extend(temp_values)
         data = data[length:]
     else:  # length_type == '1'
         # Next 11 bits represent number of sub-packets contained
         packet_count = int(data[:11], 2)
         data = data[11:]
         for i in range(packet_count):
-            data, temp_version = pop_packet(data)
+            data, temp_values, temp_version = pop_packet(data)
             version_sum += temp_version
-    return data, version_sum
+            values.extend(temp_values)
+
+    return data, values, version_sum
 
 
 def pop_packet(data):
     data, packet_version, packet_id = pop_header(data)
     version_sum = int(packet_version, 2)
-    if int(packet_id, 2) == 4:
+    id_int = int(packet_id, 2)
+    values = []
+    if id_int == 0:
+        data, values, temp_version = pop_operator(data)
+        version_sum += temp_version
+        print('Trying to sum', values)
+        values = [sum(values)]
+    elif id_int == 1:
+        data, values, temp_version = pop_operator(data)
+        version_sum += temp_version
+        product = 1
+        for value in values:
+            product *= value
+        values = [product]
+    elif id_int == 2:
+        data, values, temp_version = pop_operator(data)
+        version_sum += temp_version
+        values = [min(values)]
+    elif id_int == 3:
+        data, values, temp_version = pop_operator(data)
+        version_sum += temp_version
+        values = [max(values)]
+    elif id_int == 4:
         # Do literal stuff
         print('Literal detected:', packet_version, packet_id)
-        data, literal = pop_literal(data)
-        pass
-    else:
-        # Do operator stuff
-        print('Operator detected:', packet_version, packet_id)
-        data, temp_version = pop_operator(data)
+        data, value = pop_literal(data)
+        values.extend(value)
+    elif id_int == 5:
+        data, values, temp_version = pop_operator(data)
         version_sum += temp_version
-    return data, version_sum
+        values = [values[0] > values[1]]
+    elif id_int == 6:
+        data, values, temp_version = pop_operator(data)
+        version_sum += temp_version
+        values = [values[0] < values[1]]
+    elif id_int == 7:
+        data, values, temp_version = pop_operator(data)
+        version_sum += temp_version
+        values = [values[0] == values[1]]
+
+    else:
+        print('ERROR INCORRECT ID DETECTED')
+        data, values, temp_version = pop_operator(data)
+        version_sum += temp_version
+    return data, values, version_sum
 
 
-# input = 'A0016C880162017C3686B18A3D4780'
+# input = '9C0141080250320F1802104A08'
 
 integer = int(input, 16)
 length = 4 * len(input)
